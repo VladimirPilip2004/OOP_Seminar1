c6ce2445282b91a01980a4c5ff39c3f9bc7dc398
Vladimir Pilipenko <pilip2004@gmail.com>
2023-03-23 01:20:27 +0300

    HomeWork_01

---
+++ src/OOP_HomeWork01/Flyable.java
@@ -0,0 +1,5 @@
+package OOP_HomeWork01;
+
+public interface Flyable {
+    int getFlightSpeed();
+}
---
+++ src/OOP_HomeWork01/Main.java
@@ -0,0 +1,21 @@
+package OOP_HomeWork01;
+
+import OOP_HomeWork01.impl.Cat;
+import OOP_HomeWork01.impl.Dog;
+import OOP_HomeWork01.impl.Duck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat kat = new Cat("Тефтелька");
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(kat);
+        animals.add(new Dog("Барбос"));
+        animals.add(new Duck("Антонио"));
+
+        kat.hunt();
+    }
+}
